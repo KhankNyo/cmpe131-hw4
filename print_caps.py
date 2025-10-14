f78d771c69f514a8939905f3ade28d8eede46899
Khanh <ngokhanh809@gmail.com>
2025-10-13 18:16:54 -0700

    Added notes to 7

--- print_caps.py
+++ print_caps.py
@@ -1,9 +1,18 @@
 
-def allcaps(fn): 
+def allcaps(Fn): 
     def Wrapper(): 
-        String = fn();
+        String = Fn();
         Result = String.upper();
         return Result;
     return Wrapper;
 
+#
+## with args: 
+#def allcaps(Fn): 
+#   def Wrapper(*Args, **KWArgs): # '*' is arg by pos, '**' is args by name, or 'KeyWordArgs'
+#       String = Fn(*Args, **KWArgs);
+#       Result = String.upper();
+#       return Result;
+#   return Wrapper;
+#
 
